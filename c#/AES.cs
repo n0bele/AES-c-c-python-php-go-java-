662f7c489fa33190b89fdcb6db2e0f6ff6a53a0b
n0bele <n0bele@163.com>
2020-08-05 15:39:14 +0800

    Add files via upload

--- c#/AES.cs
+++ c#/AES.cs
@@ -1,1 +1,68 @@
-
+public class AES
+    {
+		public const string AES_KEY = "wodeshijiehenmei";
+        private const string AES_IV = "010101012345abcd";//16位
+        public static byte[] HexStringToByteArray(string s)
+        {
+            s = s.Replace(" ", "");
+            byte[] buffer = new byte[s.Length / 2];
+            for (int i = 0; i < s.Length; i += 2)
+                buffer[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
+            return buffer;
+        }
+        public static string ByteArrayToHexString(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder(data.Length * 3);
+            foreach (byte b in data)
+            {
+                //16进制数字
+                sb.Append(Convert.ToString(b, 16).PadLeft(2, '0'));
+                //16进制数字之间以空格隔开
+                //sb.Append(Convert.ToString(b, 16).PadLeft(2, '0').PadRight(3, ' '));
+            }
+            return sb.ToString().ToUpper();
+        }
+        public static string AESEnCode(string input)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(AES_KEY);
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = keyBytes;
+                aesAlg.IV = Encoding.UTF8.GetBytes(AES_IV.Substring(0, 16));
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(input);
+                        }
+                        byte[] bytes = msEncrypt.ToArray();
+                        return ByteArrayToHexString(bytes);
+                    }
+                }
+            }
+        }
+        public static string AESDeCode(string input)
+        {
+            byte[] inputBytes = HexStringToByteArray(input);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(AES_KEY);
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = keyBytes;
+                aesAlg.IV = Encoding.UTF8.GetBytes(AES_IV.Substring(0, 16));
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream(inputBytes))
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srEncrypt = new StreamReader(csEncrypt))
+                        {
+                            return srEncrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
